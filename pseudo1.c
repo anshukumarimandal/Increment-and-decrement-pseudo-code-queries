706bc87b56389a557dad1272982ec812718f3db8
Anshukumari Mandal <anshukumarimandal09@gmail.com>
2026-02-25 13:58:04 +0530

    Add files via upload

---
+++ pseudo1.c
@@ -0,0 +1,11 @@
+//  1.Increment and Decrement operations 
+#include <stdio.h>
+
+int main() {
+    int a=65;
+    int b=++a;
+    int c=((++a)-(b--));
+    int d=((a++)+(--b)+(c++));
+    printf("%d",(a+b+c+d));
+ return 0;
+ }
