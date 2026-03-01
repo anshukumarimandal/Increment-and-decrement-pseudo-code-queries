7b7d692d0f66b7dd9726c60d0767b70e0ee9dbb9
Anshukumari Mandal <anshukumarimandal09@gmail.com>
2026-03-01 14:49:19 +0530

    Add files via upload

---
+++ pseudo9.c
@@ -0,0 +1,12 @@
+// increment and decrement operator
+#include <stdio.h>
+
+int main() {
+    int a=2;
+    int b=3;
+    int c=(a++)+(++b)*(a++)/(b--)-(--a);
+    printf("a=%d\n",a);
+    printf("b=%d\n",b);
+    printf("c=%d\n",c);
+ return 0;
+}
